8ac33a5f927532af1523b7ebe901384ee5d9fbdd
Eric Dattoli <edattoli1@gmail.com>
2013-03-26 22:22:04 -0400

    10

--- MFCcontrol/Util.cs
+++ MFCcontrol/Util.cs
@@ -8,6 +8,33 @@ namespace MFCcontrol
 {
     class Util
     {
+        public class ADstorage
+        {
+            // single instance of class which is created on declaration
+            static readonly ADstorage _instance = new ADstorage();
+
+            // returns the above single instance
+            public static ADstorage Instance
+            {
+                get
+                {
+                    return _instance;
+                }
+            }
+
+            public Stack<double> inTimesQ { get; set; }
+            public Stack<double> readMFC1 { get; set; }
+            public Stack<double> readMFC2 { get; set; }
+
+            public ADstorage()
+            {
+                inTimesQ = new Stack<double>(new List<double> { });
+                readMFC1 = new Stack<double>(new List<double> { });
+                readMFC2 = new Stack<double>(new List<double> { });
+            }
+        }
+        
+        
         public async Task LongDelay()
         {
             //simulate a long delay
